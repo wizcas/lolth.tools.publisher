16215467dde5774c11a5ec20f4fa8f2d5a8a5c84
0x1C <chen@wizcas.me>
2019-05-30 17:32:08 +0800

    init commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # lolth.tools.publisher
 
 用于打包发布网站静态资源的工具库
+
+## 功能说明
+
+##### `postbuild.js` *NODE SCRIPT*
+
+用于在`yarn build`执行后处理编译生成的资源文件
+
+- 为HTML文件中的相对资源路径添加模板占位符，以便WebServer渲染时添加静态资源主机路径
+
+##### `publish` *SHELL*
+
+- 将静态资源发布到服务器，目前仅支持s3，需要预先配置aws-cli
---
+++ postbuild.js
@@ -0,0 +1,72 @@
+/**
+ * 编译后处理脚本
+ * 
+ * 用于在编译后整理HTML中的相对路径链接，添加模板占位符，
+ * 以便Web服务器渲染时将其替换成对应的CDN/S3等主机地址
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const DIST_ROOT = path.resolve(__dirname, 'dist');
+const TEMPLATE_REPLACE_TEXT = '{{ .BaseUrl }}'
+/**
+ * 正则说明：
+ * 搜索html文件中的本地url链接，并忽略：
+ * - `http:`和`https:`开头的
+ * - `//`开头的，因为之前碰到过阿里的特殊地址，形如`//g.alicdn.com/`
+ * - 形如`{{ .BaseUrl }}`的已经包含BaseUrl占位符的
+ * 
+ * 匹配结果group说明：
+ * 
+ * - `0`: Tag中含资源链接的完整Attribute，如`src="main.js"`
+ * - `1`: Attribute的开头部分，如`src="`，处理时要在它之后加上模板占位符
+ * - `2`: Attribute名称，如`src`
+ * - `4`: 实际资源链接地址，如`main.js`
+ */
+const SRC_REGEX = /((src|href)=")(?!(http:|https:|\/\/|{{ \.BaseUrl }}))(.+)"/;
+
+function makeIndexTemplate() {
+    console.log(`\x1b[33m
+--------------------------------------------------
+|               POST BUILD PROCESS               |
+--------------------------------------------------
+
+⏳ Generating index template...
+`);
+    const indexFileName = path.resolve(DIST_ROOT, 'index.html')
+    const lines = fs.readFileSync(indexFileName)
+        .toString()
+        .split('\n').map(ln => ln.trim());
+    let lineNum = 1;
+    let output = '';
+    for (let line of lines) {
+        let offset = 0;
+        let matches;
+        do {
+            const input = line.substr(offset);
+            matches = SRC_REGEX.exec(input);
+            if (matches) {
+                const replaced = matches[1];
+                const assetUrl = matches[4];
+                let replacer = replaced + TEMPLATE_REPLACE_TEXT;
+                if (!assetUrl.startsWith('/')) {
+                    // 为资源路径则加上路径分隔符
+                    replacer += '/';
+                }
+                console.log('\x1b[32mFOUND\x1b[0m', `${lineNum}:${matches.index}\t`, `${replaced}->${replacer}`);
+                input.replace(replaced, replacer);
+                line = line.substr(0, offset) + input.substr(0, matches.index) + replacer + input.substr(matches.index + replaced.length);
+                offset += matches.index + replacer.length;
+            }
+        } while (matches);
+        output += line + '\n';
+        lineNum++;
+    }
+    fs.writeFileSync(indexFileName, output);
+    console.log(`\x1b[32m
+✔️ Index template is saved. 
+`);
+}
+
+makeIndexTemplate();
